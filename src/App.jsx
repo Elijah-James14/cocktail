abe7bdda2c7b63bda95bf9dcf0d2657cc9ac1f63
Elijah James <jameselijah.1414@gmail.com>
2025-07-07 20:58:19 +0100

    hero section text and leaf animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <div className='h-dvh bg-black' />
     </main>
   );
 };
