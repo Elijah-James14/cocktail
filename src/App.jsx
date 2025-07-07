a8fa756b111975378fd156d9c9fb5cf1e5160c7a
Elijah James <jameselijah.1414@gmail.com>
2025-07-07 16:46:53 +0100

    finished working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
-  return <div className='text-6xl text-blue-800'>Web Page</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
